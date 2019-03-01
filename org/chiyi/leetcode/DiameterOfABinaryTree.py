081d2627466ecadd952e4e8b2b1ca8bae01fcc85
chyi0425 <chiyi@douyu.tv>
2019-03-01 19:18:58 +0800

    add

--- org/chiyi/leetcode/DiameterOfABinaryTree.py
+++ org/chiyi/leetcode/DiameterOfABinaryTree.py
@@ -14,10 +14,26 @@ class TreeNode:
     number f nodes along the longest path from the root node
     down to the farthest leaf node.
     """
+
+
 def height(node):
-        if node is None:
-            return 0
-        return 1 + max(height(node.left), height(node.right))
+    if node is None:
+        return 0
+    return 1 + max(height(node.left), height(node.right))
 
 
+def diameter(root):
+    if root is None:
+        return 0
 
+    # Get the height of left and right sub-trees
+    lheight = height(root.left)
+    rheight = height(root.right)
+
+    # Get the diameter of left and right sub-trees
+    ldiameter = diameter(root.left)
+    rdiameter = diameter(root.right)
+    # 1) Diameter of left tree
+    # 2) Diameter of right tree
+    # 3) Height of left subtree + height of right subtree +1
+    return max(lheight + rheight + 1, max(ldiameter, rdiameter))
