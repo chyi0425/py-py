8e1218e78d2fa1d857208a9cddf3ffbe08dc155a
chyi0425 <chyi0425@yeah.net>
2019-04-15 16:44:04 +0800

    add

--- org/chiyi/py/urllib2_test02.py
+++ org/chiyi/py/urllib2_test02.py
@@ -1,16 +1,26 @@
 # _*_ coding:UTF-8 _*_
 import requests
+import MySQLdb
 from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.67 Safari/537.36',
     }
-    target = 'https://www.xbiquge6.com/0_638/1124120.html'
+    target = 'http://10.1.100.11:9112/measures/filter/1'
     req = requests.get(url=target, headers=headers)
     req.encoding = 'UTF-8'
     html = req.text
     soup = BeautifulSoup(html, 'html.parser')
     # print(bf.prettify())
-    div_content = soup.find(name='div', id='content')
-    print(div_content.text)
+
+    t_body = soup.find(name='tbody')
+
+    div_contents = t_body.find_all(name='tr')
+    for div_content in div_contents:
+        if len(list(div_content.children))!= 17:
+            continue
+        print(div_content.find(name='a').text)
+        m_bugs = div_content.find('span', id='m_bugs').text
+        print('-----')
+        print(m_bugs)
