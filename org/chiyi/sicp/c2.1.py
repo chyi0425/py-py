02d9efb320fe072b5f336b126ca068ea73727671
chiyi <chyi0425@gmail.com>
2022-10-21 16:57:49 +0800

    add

---
+++ org/chiyi/sicp/c2.1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+
+from datetime import date
+
+today  =date(2011,9,12)
---
+++ org/chiyi/sicp/c2.6.1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# _*_ coding: utf-8 _*_
+
+
+def make_instance(cls):
+    """Return a new object instance, which is a dispatch dictionary."""
+
+    def get_value(name):
+        if name in attributes:
+            return attributes[name]
+        else:
+            value = cls['get'](name)
+            return bind_method(value, instance)
+
+    def set_value(name, value):
+        attributes[name] = value
+
+    attributes = {}
+    instance = {'get': get_value, 'set': set_value}
+    return instance
+
+
+def bind_method(value, instance):
+    """Return a bound method if value is callable, or value otherwise."""
+    if callable(value):
+        def method(*args):
+            return value(instance, *args)
+
+        return method
+    else:
+        return value
+
+
+def make_class(attributes, base_class=None):
+    """Return a new class, which is a dispatch dictionary."""
+    def get_value
